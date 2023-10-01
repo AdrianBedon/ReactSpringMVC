072905887f88fa5a31b2c519baa08355517a57f7
Adrián Bedón <arbedon2002@gmail.com>
2023-10-01 14:46:33 -0500

    Creación de la vista de lectura y eliminación de datos

---
+++ mvc/src/main/java/com/arbc/development/mvc/view/src/AppNavbar.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const AppNavbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <Navbar color="dark" dark expandable="md">
+            <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
+            <NavbarToggler onClick={() => { setIsOpen(!isOpen) }}/>
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="justify-content-end" style={{width: "100%"}} navbar>
+                    <NavItem>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    );
+};
+
+export default AppNavbar;
---
+++ mvc/src/main/java/com/arbc/development/mvc/view/src/ClientList.js
@@ -0,0 +1,80 @@
+import React, {useEffect, useState} from "react";
+import { Button, ButtonGroup, Container, Table } from "reactstrap";
+import AppNavbar from "./AppNavbar";
+import {Link} from "react-router-dom";
+
+const ClientList = () => {
+
+    const [clients, setClients] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+
+        fetch('api/clients')
+            .then(response => response.json())
+            .then(data => {
+                setClients(data);
+                setLoading(false);
+            })
+    }, []);
+
+    const remove = async (id) => {
+        await fetch(`/api/client/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/jason'
+            }
+        }).then(() => {
+            let updatedClients = [...clients].filter(i => i.id !== id);
+            setClients(updatedClients);
+        });
+    }
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    const clientList = clients.map(client => {
+        return <tr key={client.id}>
+            <td style={{whiteSpace: 'nowrap'}}>{client.username}</td>
+            <td>{client.name}</td>
+            <td>{client.lastname}</td>
+            <td>{client.email}</td>
+            <td>
+                <ButtonGroup>
+                    <Button size="sm" color="primary" tag={Link} to={"/clients/" + client.id}>Edit</Button>
+                    <Button size="sm" color="danger" onClick={() => remove(client.id)}>Delete</Button>
+                </ButtonGroup>
+            </td>
+        </tr>
+    });
+
+    return (
+        <div>
+            <AppNavbar/>
+            <Container fluid>
+                <div className="float-end">
+                    <Button color="success" tag={Link} to="/clients/new">Add Client</Button>
+                </div>
+                <h3>My Client List</h3>
+                <Table className="mt-4">
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th width="20%">Name</th>
+                            <th width="20%">Lastname</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {clientList}
+                    </tbody>
+                </Table>
+            </Container>
+        </div>
+    );
+};
+
+export default ClientList;
